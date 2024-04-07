464cdc1fff0271370fa054e515b7f5bba02e5f1d
Steven Miranda <steven.miranda@live.fr>
2024-04-07 11:40:15 +0200

    on avance bien

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,6 @@
 class List < ApplicationRecord
+  has_many :bookmarks
+  has_many :movies, through: :bookmarks, dependent: :destroy
 
-has_many :bookmarks
+  validates :name, presence: true, uniqueness: true
 end
